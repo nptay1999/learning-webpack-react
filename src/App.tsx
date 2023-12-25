7c1ab19381463d12f5721c981a4c3bc6734f644f
nptay1999 <nptay1999@gmail.com>
2023-12-25 22:46:11 +0700

    React-Typescript-Webpack: Assets management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "./assets/react.png";
+import ImageSVG from "./assets/react-logo-svg.svg";
 
 const App = () => {
-  return <h1>React Typescript Webpack starter Template</h1>;
+  return (
+    <div>
+      <h1>React Typescript Webpack starter Template</h1>
+      <img src={Image} alt="react-logo" width={300} height={300} />
+      <img src={ImageSVG} alt="react-logo" width={300} height={300} />
+    </div>
+  );
 };
 
 export default App;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "..", "src/index.tsx"),
@@ -13,16 +14,40 @@ module.exports = {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
       },
+      {
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.css$/,
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
     ],
   },
   output: {
     path: path.resolve(__dirname, "..", "dist"),
-    filename: "bundle.js",
+    filename: "script/bundle.js",
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "src/public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/index.css",
     }),
   ],
 };
